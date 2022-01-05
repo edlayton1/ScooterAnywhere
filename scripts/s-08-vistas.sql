af3101ba0f3c056f850ddfc763adf76d8fce7ba8
Juan Diaz <juanbezt@hotmail.com>
2022-01-05 16:53:45 -0600

    VIstas Lista

--- scripts/s-08-vistas.sql
+++ scripts/s-08-vistas.sql
@@ -2,8 +2,28 @@
 --@Fecha creación: 01/01/2022
 --@Descripción: Script encargado de la creacion de 3 o mas vistas
 
-create or replace view v_empleado(
-nombre,apellido_paterno,apellido_materno,clave_puesto
-) as select nombre,apellido_paterno,apellido_materno,clave_puesto
-from empleado e, puesto p
-where e.puesto_id=p.puesto_id;
+connect dpmp_proy_admin/pass
+
+create or replace view v_recarga(
+  servicio_id, tipo, scooter_id, usuario_id, clabe, banco
+) as select s.servicio_id, tipo, scooter_id, usuario_id, clabe, nombre_banco
+from servicio s, servicio_recarga sr
+where s.servicio_id = sr.servicio_id;
+
+create or replace view v_viaje(
+  servicio_id, tipo, scooter_id, usuario_id, fecha_inicio, fecha_fin, folio
+) as 
+  select s.servicio_id, tipo, scooter_id, usuario_id, fecha_inicio, fecha_fin,
+    folio
+  from servicio s, servicio_viaje sv
+  where s.servicio_id = sv.servicio_id;
+
+create or replace view v_venta(
+  servicio_id, tipo, scooter_id, usuario_id, fecha_inicio, dias, direccion
+) as 
+  select s.servicio_id, tipo, scooter_id, usuario_id, fecha_inicio, dias,
+    direccion
+  from servicio s, servicio_renta sr
+  where s.servicio_id = sr.servicio_id;
+
+
