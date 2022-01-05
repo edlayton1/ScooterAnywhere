b889c88a187e80c6bfd95d23d421162244cedb7a
Juan Diaz <juanbezt@hotmail.com>
2022-01-05 15:45:24 -0600

    Cambios a los privilegios

--- scripts/s-07-sinonimos.sql
+++ scripts/s-07-sinonimos.sql
@@ -2,4 +2,33 @@
 --@Fecha creación: 01/01/2022
 --@Descripción: Script encargado de la creacion de sinonimos del usuario admin
 
-create or replace public synonym cliente for admin.cliente;
+connect dpmp_proy_admin/pass
+
+-- 3 sinonimos publicos
+create or replace public synonym historico for dpmp_proy_admin.historico_scooter_status;
+
+create or replace public synonym zoscoo for dpmp_proy_admin.zona_scooter;
+
+create or replace public synonym tarjeta for dpmp_proy_admin.tarjeta_usuario;
+
+-- otorgar permisos de lectura 3 entidades al usuario invitado
+
+grant select on usuario to dpmp_proy_invitado;
+grant insert on usuario to dpmp_proy_invitado;
+
+grant select on falla to dpmp_proy_invitado;
+grant insert on falla to dpmp_proy_invitado;
+
+grant select on foto to dpmp_proy_invitado;
+grant insert on foto to dpmp_proy_invitado;
+
+-- 3 sinonimos del usuario invitado
+
+connect dpmp_proy_invitado/pass
+
+create or replace synonym usuario for dpmp_proy_admin.usuario;
+create or replace synonym usuario for dpmp_proy_admin.falla;
+create or replace synonym usuario for dpmp_proy_admin.foto;
+
+-- sinonimos privados 
+--xx_<nombre tabla>
